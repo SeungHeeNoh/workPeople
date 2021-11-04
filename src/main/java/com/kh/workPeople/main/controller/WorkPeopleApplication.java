beb193408d4c40eb2e1f3b5c2b35ca1c382821c6
nsh <nsh2398@naver.com>
2021-11-04 13:48:06 +0900

    #1 > project setting

---
+++ src/main/java/com/kh/workPeople/main/controller/WorkPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.kh.workPeople.main.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkPeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/workPeople/WorkPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.workPeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
