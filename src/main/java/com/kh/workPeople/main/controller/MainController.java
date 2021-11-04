4b89a6a1a2da032192e9e4e2403967996f948c4a
nsh <nsh2398@naver.com>
2021-11-04 15:04:49 +0900

    #1 > project sample setting > - 잘못 업로드된 파일 삭제

--- src/main/java/com/kh/workPeople/main/controller/MainController.java
+++ src/main/java/com/kh/workPeople/main/controller/MainController.java
@@ -2,13 +2,12 @@ package com.kh.workPeople.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class sampleController {
+public class MainController {
 
-	@GetMapping("/")
-	public String login() {
-		return "login/login";
+	@GetMapping(value= {"/", "/main"})
+	public String main() {
+		return "main/main";
 	}
 }
