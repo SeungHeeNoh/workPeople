13fe44848b267551cf72e129a0127a959e70dcaf
nsh <nsh2398@naver.com>
2021-11-04 14:32:47 +0900

    #1 > project setting

---
+++ src/main/java/com/kh/workPeople/configuration/MybatisConfiguration.java
@@ -0,0 +1,11 @@
+package com.kh.workPeople.configuration;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(basePackages="com.kh.workPeople", annotationClass=Mapper.class)
+public class MybatisConfiguration {
+	
+}
--- src/main/java/com/kh/workPeople/configuration/WorkPeopleApplication.java
+++ src/main/java/com/kh/workPeople/configuration/WorkPeopleApplication.java
@@ -1,9 +1,11 @@
-package com.kh.workPeople.main.controller;
+package com.kh.workPeople.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.kh.workPeople")
 public class WorkPeopleApplication {
 
 	public static void main(String[] args) {
