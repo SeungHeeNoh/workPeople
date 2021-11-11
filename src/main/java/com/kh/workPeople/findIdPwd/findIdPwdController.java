4ad9fca229b30d23f7094d23fa1a69bbac9d7020
nsh <nsh2398@naver.com>
2021-11-11 16:51:57 +0900

    #4 > page_findIdPwd > - page_findId, page_findpwd html 처리 - find-id-pwd.css 처리

--- src/main/java/com/kh/workPeople/findIdPwd/findIdPwdController.java
+++ src/main/java/com/kh/workPeople/findIdPwd/findIdPwdController.java
@@ -1,6 +1,7 @@
 package com.kh.workPeople.findIdPwd;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,5 +14,31 @@ public class findIdPwdController {
 		return "findIdPwd/main";
 	}
 
+	@GetMapping("personal/findId")
+	public String personalFindId(Model model) {
+		model.addAttribute("member_type", "personal");
 
+		return "findIdPwd/findId";
+	}
+	
+	@GetMapping("personal/findPwd")
+	public String personalFindPwd(Model model) {
+		model.addAttribute("member_type", "personal");
+
+		return "findIdPwd/findPwd";
+	}
+	
+	@GetMapping("company/findId")
+	public String companyFindId(Model model) {
+		model.addAttribute("member_type", "company");
+
+		return "findIdPwd/findId";
+	}
+	
+	@GetMapping("company/findPwd")
+	public String companyFindPwd(Model model) {
+		model.addAttribute("member_type", "company");
+
+		return "findIdPwd/findPwd";
+	}
 }
