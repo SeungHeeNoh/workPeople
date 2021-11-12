c763e63d0e1209ec06a9435bbd4b63fc4692c312
nsh <nsh2398@naver.com>
2021-11-12 21:35:55 +0900

    #3 > page_login > - login-common.js와 login.js를 통합(login-common.js 제거) - 아이디 저장 기능 구현 - cookie.js 구현

---
+++ src/main/resources/static/js/common/cookie.js
@@ -0,0 +1,38 @@
+(function() {
+
+	if(typeof window.common === "undefined") {
+		window.common = {};
+	}
+
+	if(typeof window.common.cookie == "undefined") {
+		window.common.cookie = {};
+	}
+
+	let cookie = window.common.cookie;
+
+	cookie.getCookie = function getCookie(cookieName) {
+		let value = "",
+			cookieIndex = document.cookie.indexOf(cookieName);
+
+		if(cookieIndex >= 0) {
+			let end = document.cookie.indexOf(";", cookieName.length);
+			if(end < 0) end = document.cookie.length;
+
+			value = document.cookie.substring(cookieIndex+cookieName.length+1, end);
+		}
+
+		return value;
+	}
+
+	cookie.setCookie = function setCookie(cookieName, value, expireDays) {
+		let expireDate = new Date();
+		expireDate.setDate(expireDate.getDate() + expireDays);
+
+		document.cookie= cookieName + "=" + escape(value) + ";" + (expireDays == null ? "" : " expires=" + expireDate.toGMTString());
+	}
+
+	cookie.deleteCookie = function deleteCookie(cookieName) {
+		document.cookie= cookieName + "= ; expires=" + new Date().toGMTString() + ";";
+	}
+
+})();
