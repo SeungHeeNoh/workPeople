4a55d359976986ac26e398a2c6ec3b5e53d5c230
Jung0731 <tjwjd5436@gmail.com>
2021-11-23 21:13:08 +0900

    companyMypage > controller 폴더 생성 후 위치 변경

--- src/main/java/com/kh/workPeople/company/mypage/companyInfo/controller/CompanyInfoController.java
+++ src/main/java/com/kh/workPeople/company/mypage/companyInfo/controller/CompanyInfoController.java
@@ -1,4 +1,4 @@
-package com.kh.workPeople.company.mypage.companyInfo;
+package com.kh.workPeople.company.mypage.companyInfo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
