f7cf86f92c03b9a7be4307dea2d421b722a9f148
Jung0731 <tjwjd5436@gmail.com>
2021-11-23 22:50:54 +0900

    page_companyInfo > mapper,service 연결

--- src/main/java/com/kh/workPeople/company/mypage/companyInfo/controller/CompanyInfoController.java
+++ src/main/java/com/kh/workPeople/company/mypage/companyInfo/controller/CompanyInfoController.java
@@ -1,15 +1,38 @@
 package com.kh.workPeople.company.mypage.companyInfo.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.workPeople.common.vo.Member;
+import com.kh.workPeople.company.mypage.companyInfo.serivce.CompanyInfoService;
 
 @Controller
 @RequestMapping("/company/mypage")
 public class CompanyInfoController {
 	
+	private CompanyInfoService companyInfoService;
+	
+	
+	@Autowired
+	public CompanyInfoController(CompanyInfoService companyInfoService) {
+		this.companyInfoService = companyInfoService;
+	}
+
 	@GetMapping("/companyInfoInsert")
-	public String companyInfo() {
+	public String companyInfo(Member member) {
+		
+		companyInfoService.companyInfoSelect(member);
+;		
+		return "company/mypage/companyInfoInsert";
+	}
+	
+	@PostMapping("/companyInfoInsert")
+	public String companyInfoInsert() {
 		return "company/mypage/companyInfoInsert";
 	}
 	
