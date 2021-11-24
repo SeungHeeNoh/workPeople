27ef070335e3a4dbd38b10bb81458403f09ae8ce
Jung0731 <tjwjd5436@gmail.com>
2021-11-25 00:03:57 +0900

    page_companyInfoInsert > 기업정보입력 작성(1차작업/로고업로드 미작성)

--- src/main/java/com/kh/workPeople/common/vo/CompanyInformation.java
+++ src/main/java/com/kh/workPeople/common/vo/CompanyInformation.java
@@ -2,11 +2,14 @@ package com.kh.workPeople.common.vo;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class CompanyInformation {
 
 	private int no;
 	private String registerNumber;
 	private String ceoName;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date foundingDate;
 	private int employeeNumber;
 	private String businessDetail;
@@ -18,6 +21,8 @@ public class CompanyInformation {
 	private CompanyType companyType;
 	
 	public CompanyInformation() {}
+
+	
 
 	public CompanyInformation(int no, String registerNumber, String ceoName, Date foundingDate, int employeeNumber,
 			String businessDetail, String companyHistory, String companyVision, String homepage, Attachment attachment,
@@ -37,109 +42,163 @@ public class CompanyInformation {
 		this.companyType = companyType;
 	}
 
+
+
 	public int getNo() {
 		return no;
 	}
+
+
 
 	public void setNo(int no) {
 		this.no = no;
 	}
 
+
+
 	public String getRegisterNumber() {
 		return registerNumber;
 	}
+
+
 
 	public void setRegisterNumber(String registerNumber) {
 		this.registerNumber = registerNumber;
 	}
 
+
+
 	public String getCeoName() {
 		return ceoName;
 	}
+
+
 
 	public void setCeoName(String ceoName) {
 		this.ceoName = ceoName;
 	}
 
+
+
 	public Date getFoundingDate() {
 		return foundingDate;
 	}
+
+
 
 	public void setFoundingDate(Date foundingDate) {
 		this.foundingDate = foundingDate;
 	}
 
+
+
 	public int getEmployeeNumber() {
 		return employeeNumber;
 	}
+
+
 
 	public void setEmployeeNumber(int employeeNumber) {
 		this.employeeNumber = employeeNumber;
 	}
 
+
+
 	public String getBusinessDetail() {
 		return businessDetail;
 	}
+
+
 
 	public void setBusinessDetail(String businessDetail) {
 		this.businessDetail = businessDetail;
 	}
 
+
+
 	public String getCompanyHistory() {
 		return companyHistory;
 	}
+
+
 
 	public void setCompanyHistory(String companyHistory) {
 		this.companyHistory = companyHistory;
 	}
 
+
+
 	public String getCompanyVision() {
 		return companyVision;
 	}
+
+
 
 	public void setCompanyVision(String companyVision) {
 		this.companyVision = companyVision;
 	}
 
+
+
 	public String getHomepage() {
 		return homepage;
 	}
+
+
 
 	public void setHomepage(String homepage) {
 		this.homepage = homepage;
 	}
 
+
+
 	public Attachment getAttachment() {
 		return attachment;
 	}
+
+
 
 	public void setAttachment(Attachment attachment) {
 		this.attachment = attachment;
 	}
 
+
+
 	public Sector getSector() {
 		return sector;
 	}
+
+
 
 	public void setSector(Sector sector) {
 		this.sector = sector;
 	}
 
+
+
 	public CompanyType getCompanyType() {
 		return companyType;
 	}
+
+
 
 	public void setCompanyType(CompanyType companyType) {
 		this.companyType = companyType;
 	}
 
+
+
 	@Override
 	public String toString() {
-		return "CompanyInfo [no=" + no + ", registerNumber=" + registerNumber + ", ceoName=" + ceoName
+		return "CompanyInformation [no=" + no + ", registerNumber=" + registerNumber + ", ceoName=" + ceoName
 				+ ", foundingDate=" + foundingDate + ", employeeNumber=" + employeeNumber + ", businessDetail="
 				+ businessDetail + ", companyHistory=" + companyHistory + ", companyVision=" + companyVision
 				+ ", homepage=" + homepage + ", attachment=" + attachment + ", sector=" + sector + ", companyType="
 				+ companyType + "]";
 	}
-	
+
+
+
+
+
 }
