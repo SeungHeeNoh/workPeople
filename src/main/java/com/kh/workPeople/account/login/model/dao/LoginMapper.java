288b994e31f1d64643092726d44f4d07afd21fe2
nsh <nsh2398@naver.com>
2021-11-25 18:46:35 +0900

    #25 > page_login > - 바뀐 경로에 맞게 import 변경

--- src/main/java/com/kh/workPeople/account/login/model/dao/LoginMapper.java
+++ src/main/java/com/kh/workPeople/account/login/model/dao/LoginMapper.java
@@ -2,9 +2,9 @@ package com.kh.workPeople.account.login.model.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.kh.workPeople.common.vo.Manager;
 import com.kh.workPeople.common.vo.Member;
 import com.kh.workPeople.common.vo.MemberImpl;
-import com.kh.workPeople.manager.model.vo.Manager;
 
 @Mapper
 public interface LoginMapper {
