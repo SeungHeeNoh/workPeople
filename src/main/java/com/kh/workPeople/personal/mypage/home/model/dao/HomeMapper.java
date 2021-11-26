212d265f2fede97b48c859e9dc4b043ff3dfcbf0
JoYongHo <adauy@daum.net>
2021-11-26 13:44:37 +0900

    오류관련 작업내용 커밋중

--- src/main/java/com/kh/workPeople/personal/mypage/home/model/dao/HomeMapper.java
+++ src/main/java/com/kh/workPeople/personal/mypage/home/model/dao/HomeMapper.java
@@ -1,11 +1,27 @@
 package com.kh.workPeople.personal.mypage.home.model.dao;
 
+import com.kh.workPeople.common.vo.JobVacancyLookUp;
 import com.kh.workPeople.common.vo.Resume;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface HomeMapper {
 
     Resume selectResumeStatusY(int no);
+
+    int applyCount(int no);
+
+    int resumeBrowseCount(int no);
+
+    int scrapCount(int no);
+
+    int interestedCompanyCount(int no);
+
+    int chatCount(int no);
+
+    List<JobVacancyLookUp> recommenedJobVacancyList(String elName);
+
 
 }
