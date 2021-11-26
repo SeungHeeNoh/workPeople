5b8b539fb81736c2bcb113fbda1263b178e32f2e
JoYongHo <adauy@daum.net>
2021-11-26 13:45:58 +0900

    오류관련 작업내용 커밋중

--- src/main/java/com/kh/workPeople/personal/mypage/home/model/service/HomeServiceImpl.java
+++ src/main/java/com/kh/workPeople/personal/mypage/home/model/service/HomeServiceImpl.java
@@ -1,23 +1,55 @@
 package com.kh.workPeople.personal.mypage.home.model.service;
 
+import com.kh.workPeople.common.vo.JobVacancyLookUp;
 import com.kh.workPeople.common.vo.Resume;
 import com.kh.workPeople.personal.mypage.home.model.dao.HomeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class HomeServiceImpl implements HomeService {
 
-    private HomeMapper homeMapper;
+    private final HomeMapper homeMapper;
 
     @Autowired
     public HomeServiceImpl(HomeMapper homeMapper){
-       this.homeMapper = homeMapper;
+        this.homeMapper = homeMapper;
     }
 
 
     @Override
     public Resume selectResumeStatusY(int no) {
         return homeMapper.selectResumeStatusY(no);
+    }
+
+    @Override
+    public int applyCount(int no) {
+        return homeMapper.applyCount(no);
+    }
+
+    @Override
+    public int resumeBrowseCount(int no) {
+        return homeMapper.resumeBrowseCount(no);
+    }
+
+    @Override
+    public int scrapCount(int no) {
+        return homeMapper.scrapCount(no);
+    }
+
+    @Override
+    public int interestedCompanyCount(int no) {
+        return homeMapper.interestedCompanyCount(no);
+    }
+
+    @Override
+    public int chatCount(int no) {
+        return homeMapper.chatCount(no);
+    }
+    @Override
+    public List<JobVacancyLookUp> recommenedJobVacancyList(String elName) {
+        return homeMapper.recommenedJobVacancyList(elName);
     }
 }
