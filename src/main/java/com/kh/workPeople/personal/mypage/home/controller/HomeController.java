ca34af6639b7e477d23b2ca6a5b5466a83f77bd2
JoYongHo <adauy@daum.net>
2021-11-26 13:43:28 +0900

    오류관련 작업내용 커밋중

--- src/main/java/com/kh/workPeople/personal/mypage/home/controller/HomeController.java
+++ src/main/java/com/kh/workPeople/personal/mypage/home/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.kh.workPeople.personal.mypage.home.controller;
 
+import com.kh.workPeople.common.vo.JobVacancyLookUp;
 import com.kh.workPeople.common.vo.MemberImpl;
 import com.kh.workPeople.common.vo.Resume;
 import com.kh.workPeople.personal.mypage.home.model.service.HomeService;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/personal/mypage")
@@ -27,19 +29,39 @@ public class HomeController {
 	@GetMapping("home")
 	public String mypageHome(Model model, @AuthenticationPrincipal MemberImpl user) {
 
+		int applyCount = homeService.applyCount(user.getNo());
+		int resumeBrowseCount = homeService.resumeBrowseCount(user.getNo());
+		int scrapCount = homeService.scrapCount(user.getNo());
+		int interestedCompanyCount = homeService.interestedCompanyCount(user.getNo());
+		int chatCount = homeService.chatCount(user.getNo());
+
+		model.addAttribute("applyCount",applyCount);
+		model.addAttribute("resumeBrowseCount",resumeBrowseCount);
+		model.addAttribute("scrapCount",scrapCount);
+		model.addAttribute("interestedCompanyCount",interestedCompanyCount);
+		model.addAttribute("chatCount",chatCount);
+
+
 		Resume resume = homeService.selectResumeStatusY(user.getNo());
+		String elName = null;
 
-		Date beforeDate = resume.getEnrollDate();
+		if(resume != null) {
+			Date beforeDate = resume.getEnrollDate();
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY.MM.dd");
+			String afterDate = simpleDateFormat.format(beforeDate);
+			elName = resume.getS_name();
+			model.addAttribute("resumeEnrollDate", afterDate);
+			model.addAttribute("resume", resume);
+		}
 
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY.MM.dd");
-		String afterDate = simpleDateFormat.format(beforeDate);
-		
-		model.addAttribute("resumeEnrollDate",afterDate);
-		model.addAttribute("resume",resume);
+		List<JobVacancyLookUp> jobVacancyLookUpList = homeService.recommenedJobVacancyList(elName);
+
+		model.addAttribute("jobVacancyLookUpList",jobVacancyLookUpList);
 
 		return "personal/mypage/home";
-	} 
-	
+	}
+
+
 
 
 
