9bf27e0b9be3e58639d36c3fff019b5349948aac
Jung0731 <tjwjd5436@gmail.com>
2021-11-29 09:58:28 +0900

    page_personInfoList > 맞춤 인재 추천 기능 개발(1차)

---
+++ src/main/java/com/kh/workPeople/company/mypage/talented/service/TalentedService.java
@@ -0,0 +1,12 @@
+package com.kh.workPeople.company.mypage.talented.service;
+
+import java.util.Map;
+
+public interface TalentedService {
+
+	Map<String, Object> findList(int page, int userNo);
+
+
+	
+
+}
