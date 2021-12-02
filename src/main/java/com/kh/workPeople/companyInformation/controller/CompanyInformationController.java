03bbecfee5221032683dc184d96a016e43426435
nsh <nsh2398@naver.com>
2021-12-02 09:32:49 +0900

    #28 > page_companyInformation > Controller의 전달 방식 변경

--- src/main/java/com/kh/workPeople/companyInformation/controller/CompanyInformationController.java
+++ src/main/java/com/kh/workPeople/companyInformation/controller/CompanyInformationController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.workPeople.companyInformation.model.service.CompanyInformationService;
@@ -20,17 +21,20 @@ public class CompanyInformationController {
 	private CompanyInformationService companyInformationService;
 
 	@GetMapping("/detail-view")
-	public String detailView(@RequestParam int no, Model model, RedirectAttributes rttr) {
+	public ModelAndView detailView(@RequestParam(defaultValue="0") int no, Model model) {
 		Map<String, Object> map = companyInformationService.getCompanyInformation(no);
+		ModelAndView mv = new ModelAndView();
 		
 		if(map.get("member") != null && map.get("companyInformation") != null) {
-			model.addAttribute("member", map.get("member"));
-			model.addAttribute("companyInformation", map.get("companyInformation"));
-			model.addAttribute("newLineChar", "\n");
-			return "company-information/detail-view"; 
+			mv.addObject("member", map.get("member"));
+			mv.addObject("companyInformation", map.get("companyInformation"));
+			mv.addObject("newLineChar", "\n");
+			mv.setViewName("company-information/detail-view");
 		} else {
-			rttr.addFlashAttribute("errorMessage", "잘못된 페이지 접근입니다.");
-			return "redirect:/common/errorPage";
+			mv.addObject("errorMessage", "기업 정보 상세 조회에 실패했습니다.");
+			mv.setViewName("/common/errorPage");
 		}
+
+		return mv;
 	}
 }
