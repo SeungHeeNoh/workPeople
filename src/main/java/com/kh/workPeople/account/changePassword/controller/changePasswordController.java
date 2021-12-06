ba9aa725eb5192668f8e58538032066de47506e8
nsh <nsh2398@naver.com>
2021-12-06 17:31:35 +0900

    #88 > spring security > change-password페이지는 로그인 후 임시 비밀번호인 케이스만 접근하도록 변경

--- src/main/java/com/kh/workPeople/account/changePassword/controller/changePasswordController.java
+++ src/main/java/com/kh/workPeople/account/changePassword/controller/changePasswordController.java
@@ -1,6 +1,6 @@
 package com.kh.workPeople.account.changePassword.controller;
 
-
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.workPeople.account.changePassword.model.service.ChangePasswordService;
 import com.kh.workPeople.common.vo.MemberImpl;
@@ -21,20 +20,28 @@ public class changePasswordController {
 	private ChangePasswordService changePasswordService;
 
 	@GetMapping("/change-password")
-	public String changePassword() {
-		return "account/change-password/change-password";
+	public String changePassword(@AuthenticationPrincipal MemberImpl memberImpl, HttpServletRequest request) {
+		String page = "account/change-password/change-password";
+		
+		if(!memberImpl.getTempPwdYn().equals("Y")) {
+			request.setAttribute("errorMessage", "잘못된 접근입니다.");
+			page = "/common/errorPage";
+		}
+		
+		return page;
 	}
 	
 	@PostMapping("/change-password")
-	public String updatePassword(@AuthenticationPrincipal MemberImpl memberImpl, String password, RedirectAttributes rttr) {
+	public String updatePassword(@AuthenticationPrincipal MemberImpl memberImpl, String password, HttpServletRequest request) {
+		String page = "account/change-password/change-password";
 		int result = changePasswordService.changePassword(memberImpl, password);
 		
-		if(result > 0) {
-			return "redirect:/";
-		} else {
-			rttr.addFlashAttribute("message", "비밀번호 변경에 실패하였습니다.");
-			return "redirect:/common/errorPage";
+		if(result <= 0) {
+			request.setAttribute("errorMessage", "비밀번호 변경에 실패하였습니다.");
+			page = "/common/errorPage";
 		}
+		
+		return page;
 	}
 	
 }
