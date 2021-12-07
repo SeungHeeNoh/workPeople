49cd7b4dc70aa49041acd5efded6a7c2f7b625b2
nsh <nsh2398@naver.com>
2021-12-07 22:52:15 +0900

    #27 > page_JobVacancyDetail > 이력서 수정/미리보기 기능 연결

--- src/main/java/com/kh/workPeople/jobs/vacancyDetail/controller/VacancyDetailController.java
+++ src/main/java/com/kh/workPeople/jobs/vacancyDetail/controller/VacancyDetailController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +21,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.kh.workPeople.common.vo.Activity;
+import com.kh.workPeople.common.vo.Awards;
+import com.kh.workPeople.common.vo.Career;
+import com.kh.workPeople.common.vo.Language;
+import com.kh.workPeople.common.vo.License;
 import com.kh.workPeople.common.vo.MemberImpl;
+import com.kh.workPeople.common.vo.ResumeDetails;
+import com.kh.workPeople.common.vo.SelfIntroduction;
 import com.kh.workPeople.jobs.vacancyDetail.model.service.VacancyDetailService;
 import com.kh.workPeople.jobs.vacancyDetail.model.vo.JobVacancyInformation;
 import com.kh.workPeople.personal.mypage.applyCompany.model.service.ApplyCompanyService;
 import com.kh.workPeople.personal.mypage.latestPublication.model.service.LatestPublicationService;
+import com.kh.workPeople.personal.mypage.resume.model.service.ResumeService;
 
 @Controller
 @RequestMapping("/jobs/vacancy-detail")
@@ -36,6 +45,8 @@ public class VacancyDetailController {
 	private LatestPublicationService latestPublicationService;
 	@Autowired
 	private ApplyCompanyService applyCompanyService;
+	@Autowired
+	private ResumeService resumeService;
 	
 	@GetMapping("/detail-view")
 	public ModelAndView vacancyDetail(HttpServletRequest request, HttpServletResponse response, @RequestParam(defaultValue = "0") int no, @AuthenticationPrincipal UserDetails user) {
@@ -120,6 +131,63 @@ public class VacancyDetailController {
 		}
 		
 		return page;
+	}
+	
+	@GetMapping("/resume-view")
+	public String resumeView(@RequestParam int rNo, Model model) {
+		ResumeDetails resumeDetails = resumeService.resumeDetailsLookUp(rNo);
+		// 이력서 정보 조회(기본정보,학력의 나이->만나이,한국나이 포멧, util.Date->string 포멧(YYYY.MM)
+		ResumeDetails resumeDetailsFormat = resumeService.resumeDetailsLookUpFormat(rNo);
+		// 포멧팅해서 조회하고 불러온 객체 -> 상세페이지 객체에 할당
+		resumeDetails.setBiBirthDateYearFormat(resumeDetailsFormat.getBiBirthDateYearFormat());
+		resumeDetails.setBiAge(resumeDetailsFormat.getBiAge());
+		resumeDetails.setBiAgeInFull(resumeDetailsFormat.getBiAgeInFull());
+		resumeDetails.seteHighAdmissionFormat(resumeDetailsFormat.geteHighAdmissionFormat());
+		resumeDetails.seteHighGraduationFormat(resumeDetailsFormat.geteHighGraduationFormat());
+		resumeDetails.seteColleageAdmissionFormat(resumeDetailsFormat.geteColleageAdmissionFormat());
+		resumeDetails.seteColleageGraduateFormat(resumeDetailsFormat.geteColleageGraduateFormat());
+		resumeDetails.seteMasterAdmissionFormat(resumeDetailsFormat.geteMasterAdmissionFormat());
+		resumeDetails.seteMasterGraduateFormat(resumeDetailsFormat.geteMasterGraduateFormat());
+		resumeDetails.seteDoctorAdmissionFormat(resumeDetailsFormat.geteDoctorAdmissionFormat());
+		resumeDetails.seteDoctorGraduateFormat(resumeDetailsFormat.geteDoctorGraduateFormat());
+
+		// 포멧팅된 값까지 입력된 기본정보/학력의 모든 정보가 담긴 객체 front로 넘기기
+		model.addAttribute("resumeDetails",resumeDetails);
+
+		// 학력(고졸미만, 고졸, 검정고시, 대졸이상)에 따라 학력 출력하기위한 조건
+		String resumeEtype = resumeDetails.geteType();
+		String resumeColleageType = resumeDetails.geteColleageType();
+		String resumeMasterName = resumeDetails.geteMasterName();
+		String resumeDoctorName = resumeDetails.geteDoctorName();
+
+		// 학력 출력시 필요한 조건 프론트로 넘기기
+		model.addAttribute("resumeEtype",resumeEtype);
+		model.addAttribute("resumeColleageType",resumeColleageType);
+		model.addAttribute("resumeMasterName",resumeMasterName);
+		model.addAttribute("resumeDoctorName",resumeDoctorName);
+
+		// 경력 리스트 조회
+		List<Career> resumeCareerList = resumeService.resumeCareerList(rNo);
+		// 경력 리스트 프론트로 넘기기
+		model.addAttribute("resumeCareerList",resumeCareerList);
+
+		// 인턴, 대외활동 목록 조회
+		List<Activity> resumeActivityList = resumeService.resumeActivityList(rNo);
+		model.addAttribute("resumeActivityList",resumeActivityList);
+
+		// 자격증/어학/수상내역 목록 조회
+		List<License> resumeLicenseList = resumeService.resumeLicenseList(rNo);
+		List<Language> resumeLanguageList = resumeService.resumeLanguageList(rNo);
+		List<Awards> resumeAwardsList = resumeService.resumeAwardsList(rNo);
+		model.addAttribute("resumeLicenseList",resumeLicenseList);
+		model.addAttribute("resumeLanguageList",resumeLanguageList);
+		model.addAttribute("resumeAwardsList",resumeAwardsList);
+
+		// 자소서 항목 목록 조회
+		List<SelfIntroduction> resumeSelfIntroductionList = resumeService.resumeSelfIntroductionList(rNo);
+		model.addAttribute("resumeSelfIntroductionList",resumeSelfIntroductionList);
+			
+		return "jobs/common/resumeView";
 	}
 
 	protected boolean isPersonalUser(UserDetails user) {
